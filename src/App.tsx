4a321322a73a753ba34a6e11474e4e4c4434edbd
Shantanu <shantanukorgaonkar7@gmail.com>
2024-05-05 21:47:23 +0530

    add container min height

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { Grid, Typography } from '@mui/material';
 import Dashboard from './components/Dashboard';
 
-
 function App() {
   return (
-    <Grid container justifyContent="center" gap={4} >
+    <Grid container justifyContent="center" gap={4} height="100%">
       <Grid item xs={12} >
         <Typography variant='h2' textAlign="center">Asteroid Dashboard</Typography>
       </Grid>
-      <Grid item xs={10} sx={{ backgroundColor: '#FFFFFF', padding: 4 }}>
+      <Grid item xs={10} sx={{ backgroundColor: '#FFFFFF', padding: 4, minHeight:600 }}>
         <Dashboard />
       </Grid>
     </Grid>
